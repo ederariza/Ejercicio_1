a8d5613db7bd2e9c9e4ce98c3698acf0cb66ca19
Infromatica II <eder.ariza@udea.edu.co>
2020-09-22 12:23:57 -0500

    Ejercicio 1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(){
+
+    int A, B, C;
+    cout<< "Ingrese el numero A: ";
+    cin>> A;
+    cout<< "Ingrese el numero B: ";
+    cin>> B;
+    C = A % B;
+    cout << "El modulo es: " << C << endl;
     return 0;
+
 }
